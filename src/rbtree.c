72c7cddc3b368f82a9b2d29951612d3803e2ba44
sjlee <asyncwaiter@gmail.com>
2024-09-03 10:01:21 +0000

    Add Rotate 함수

--- src/rbtree.c
+++ src/rbtree.c
@@ -31,6 +31,45 @@ node_t *rbtree_find(const rbtree *t, const key_t key) {
   return NULL;
 }
 
+void left_rotate(rbtree *t, node_t *x){
+  node_t *y = x->right;
+  x->right = y->left;
+  
+  if (y->left != t->nil)
+    y->left->parent = x;
+  y->parent = x->parent;
+  
+  if(x->parent == t->nil)
+    t->root = y;
+  else if (x == x->parent->left)
+    x->parent->left = y;
+  else
+    x->parent->right = y;
+
+  y->left = x;
+  x->parent = y;
+}
+
+void right_rotate(rbtree *t, node_t *x){
+	node_t *y = x->left;
+	x->left = y->left;
+
+	if (y->right != t->nil)
+    y->right->parent = x;
+  y->parent = x->parent;
+  
+	if (x->parent == t->nil)
+    t->root = y;
+	else if (x == x->parent->left)
+    x->parent->left = y;
+  else
+    x->parent->right = y;
+
+  y->right = x;
+  x->parent = y;
+			
+}
+
 node_t *rbtree_min(const rbtree *t) {
   // TODO: implement find
   return t->root;
